da6eeaf95ed1dbf1f1c7add2558d0c617eb3c9ed
Ritesh Gorle <94937498+RiteshGorle@users.noreply.github.com>
2022-06-15 23:21:24 +0530

    BRYS 2

---
+++ script.js
@@ -0,0 +1,28 @@
+/*
+SIDE NAVIGATION
+*/
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
+
+
+
+
+/*
+MULTI LEVEL DROPDOWN
+*/
+
+$(document).ready(function(){
+  $('.dropdown-submenu a.test').on("click", function(e){
+    $(this).next('ul').toggle();
+    e.stopPropagation();
+    e.preventDefault();
+  });
+});
